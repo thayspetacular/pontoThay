9a793e5f6da8b6b6ec2ff7562e9680c1841f2079
thayspetacular <145724358+thayspetacular@users.noreply.github.com>
2024-06-14 22:00:22 -0300

    Add files via upload

--- login.js
+++ login.js
@@ -15,3 +15,23 @@ document.addEventListener('DOMContentLoaded', function() {
             });
     });
 });
+
+
+
+// Login de administrador
+document.getElementById('adminLoginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const adminEmail = document.getElementById('adminEmail').value;
+    const adminSenha = document.getElementById('adminSenha').value;
+
+    // Implemente a lógica de autenticação para administradores usando Firebase Auth
+    firebase.auth().signInWithEmailAndPassword(adminEmail, adminSenha)
+        .then((userCredential) => {
+            // Login bem-sucedido para administrador
+            window.location.href = 'administracao.html'; // Redireciona para a página de administração
+        })
+        .catch((error) => {
+            console.error('Erro ao fazer login como administrador: ', error.message);
+            alert('Falha ao entrar como administrador: ' + error.message);
+        });
+});
